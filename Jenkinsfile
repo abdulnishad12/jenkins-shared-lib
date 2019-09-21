576a58a5d9a1dac2e9ecf9641619aa0257624553
abdulnishad12 <abdulnishad12@gmail.com>
2019-09-21 15:31:12 +0530

    final

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,15 +1,13 @@
 @Library('jenkins-shared-lib@master') _
- 
-pipeline {
+ pipeline {
     agent any
     stages {
         stage('Generate Json File') {
             steps {
             generateJson(
-                credentials_file: "127.0.0.1 abd cef",
-                
-            )
+                credentials_file: "127.0.0.1 abd cef"
+                )
             }
+        }
     }
-    }
-}
+ }
--- vars/generateJson.groovy
+++ vars/generateJson.groovy
@@ -2,8 +2,10 @@ def call(Map stageParams) {
  def cred=stageParams.credentials_file
  String[] splitData = cred.split("\n");
  for (String eachSplit : splitData) {
- def line = "${eachSplit}".split()
- def pass = powershell label: '', returnStatus: true, script: ".\\decrypt.ps1 ${line[0]}.encrypt"
-def json = withEnv(["VAR_A=${line[0]}","VAR_B=${pass}"]){ powershell label: '', script: ''' @{ ip= "$env:VAR_B"; domain= "block64.local"; username= "Administrator"; password= "$env:VAR_A"; system_type= "win"; timeout= 90 } | ConvertTo-Json -Compress > api.json '''
+  def line = "${eachSplit}".split()
+  def pass = powershell label: '', returnStatus: true, script: ".\\decrypt.ps1 ${line[0]}.encrypt"
+  def json = withEnv(["VAR_A=${line[0]}", "VAR_B=${pass}"]) {
+   powershell label: '', script: ''' @{ ip= "$env:VAR_B"; domain= "block64.local"; username= "Administrator"; password= "$env:VAR_A"; system_type= "win"; timeout= 90 } | ConvertTo-Json -Compress > api.json '''
 
   }
+ }}
