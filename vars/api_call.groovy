d90024921829920db749e98e6b011d02e497ce2f
Nishad A <abdulnishad12@gmail.com>
2019-09-29 18:35:19 +0530

    Update api_call.groovy

--- vars/api_call.groovy
+++ vars/api_call.groovy
@@ -2,6 +2,6 @@ def call(Map stageParams) {
 def api_json = stageParams.api_json
 def file = readFile('api.json')
  def str = file.substring(2)
- str = \'''+str+\'''
+ str = '''+str+'''
 print (str)
 }
