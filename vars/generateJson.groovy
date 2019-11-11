27816e473b6200fef2c1a09a55c9a689f1f95d1f
Nishad A <abdulnishad12@gmail.com>
2019-11-12 02:46:09 +0530

    Update generateJson.groovy

--- vars/generateJson.groovy
+++ vars/generateJson.groovy
@@ -1,5 +1,5 @@
 def call(Map stageParams) {
 def str = api_call
   Object.metaClass.printit { -> println org.codehaus.groovy.runtime.InvokerHelper.toString(delegate) }
-  str.printit()
+  dump(str)
 }
